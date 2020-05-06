b5b838bc4e994c1d763bc2273ece68e4744667e6
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-06 15:07:57 -0300

    Creating user route and service

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: '😊 Welcome!' });
 });
+
 export default routes;
