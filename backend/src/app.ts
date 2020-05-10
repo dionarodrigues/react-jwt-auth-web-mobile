1a34463ebb068fa2ca0fad49068e159679aea97e
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-10 20:46:10 -0300

    Creating a route to serve statics files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 export default app;
