c9315bad99e94d523764d420e57badb2bc69e103
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-14 09:46:15 -0300

    Using @unform to handle form/input

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -7,11 +8,15 @@ import Input from '../../components/Input';
 import * as S from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <S.Container>
       <S.Content>
         <h1>Create an account</h1>
-        <form>
+        <Form onSubmit={handleSubmit}>
           <Input placeholder="Name" icon={FiUser} name="name" type="text" />
           <Input placeholder="Email" icon={FiMail} name="email" type="text" />
           <Input
@@ -21,7 +26,7 @@ const SignUp: React.FC = () => {
             type="password"
           />
           <Button type="submit">Sign Up</Button>
-        </form>
+        </Form>
         <a href="/">
           <FiArrowLeft size={13} />
           <span>Do you have an account?</span>
