fcb6156ca86e3bc5f066112eeb8eba6101c08ef5
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-18 13:43:49 -0300

    Fixing SignIn form

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -7,11 +8,15 @@ import Input from '../../components/Input';
 import * as S from './styles';
 
 const SignIn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <S.Container>
       <S.Content>
         <h1>Sign In</h1>
-        <form>
+        <Form onSubmit={handleSubmit}>
           <Input placeholder="Email" icon={FiMail} name="email" type="text" />
           <Input
             placeholder="Password"
@@ -20,7 +25,7 @@ const SignIn: React.FC = () => {
             type="password"
           />
           <Button type="submit">Entrar</Button>
-        </form>
+        </Form>
         <a href="/">
           <FiLogIn size={13} />
           <span>New account</span>
