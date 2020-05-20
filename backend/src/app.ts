2451bb273e997d7b1054b553ec77484b7cd8872a
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-20 09:44:14 -0300

    adding cors to Back-end application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import uploadConfig from './config/upload';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
