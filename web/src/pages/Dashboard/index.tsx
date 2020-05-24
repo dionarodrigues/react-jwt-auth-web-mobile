e28cea52db491dd28ff645af305166974bc90c46
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-23 23:29:33 -0300

    Adding Dashboard page

---
+++ web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>Dashboard</h1>
+    </S.Container>
+  );
+};
+
+export default Dashboard;
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" component={SignIn} exact />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} />
   </Switch>
 );
 
