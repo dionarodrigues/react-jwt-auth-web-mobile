e4291da2ae2513225c301c6218cf7b04597b0bd9
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-28 09:37:07 -0300

    Adding fonts and text into the project

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { Container } from './styles';
+import * as S from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <S.Container>
+      <S.Title>Sign In</S.Title>
+    </S.Container>
+  );
 };
 
 export default SignIn;
--- app/src/pages/SignIn/styles.ts
+++ app/src/pages/SignIn/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'Roboto-Regular';
+  margin: 65px 0 25px;
+`;
