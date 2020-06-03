ae6af44b28b71ac529c2f33454bae2135cf4a7d1
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-06-03 00:54:07 -0300

    Finishing Sign In Styles

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -7,14 +9,40 @@ import * as S from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <S.Container>
-      <S.Title>Sign In</S.Title>
+    <>
+      {/* KeyboardAvoidingView makes the keyboard works correctly when opened */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        {/* ScrollView allows scrolling and close the keyboard when click out of input */}
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <S.Container>
+            <View>
+              <S.Title>Sign In</S.Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="Email" />
-      <Input name="password" icon="lock" placeholder="Password" />
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Password" />
 
-      <Button onPress={() => {}}>Enter</Button>
-    </S.Container>
+            <Button onPress={() => {}}>Enter</Button>
+
+            <S.ForgotPasswordButton onPress={() => {}}>
+              <S.ForgotPasswordText>Forgot Password</S.ForgotPasswordText>
+            </S.ForgotPasswordButton>
+          </S.Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <S.CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#0070ba" />
+        <S.CreateAccountText>New account</S.CreateAccountText>
+      </S.CreateAccountButton>
+    </>
   );
 };
 
--- app/src/pages/SignIn/styles.ts
+++ app/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +13,39 @@ export const Title = styled.Text`
   color: #0e0d11;
   font-family: 'Roboto-Regular';
   margin: 65px 0 25px;
+`;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #555;
+  font-size: 16px;
+  font-family: 'Roboto-Medium';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  background: #0070ba;
+  color: #0e0d11;
+  background: #fff;
+  border-top-width: 1px;
+  border-color: #dfdfdf;
+  width: 100%;
+
+  /* Setting a dinamic space for iphones */
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const CreateAccountText = styled.Text`
+  color: #0070ba;
+  font-size: 16px;
+  font-family: 'Roboto-Medium';
+  margin-left: 10px;
 `;
