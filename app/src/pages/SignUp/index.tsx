d3143dac195979c0f620e32dc0ec0c41ea73422f
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-06-03 09:59:20 -0300

    Improving SignUp Page and adding styles

--- app/src/pages/SignUp/index.tsx
+++ app/src/pages/SignUp/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import * as S from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      {/* KeyboardAvoidingView makes the keyboard works correctly when opened */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        {/* ScrollView allows scrolling and close the keyboard when click out of input */}
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <S.Container>
+            <View>
+              <S.Title>Create an account</S.Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Name" />
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button onPress={() => {}}>Sign Up</Button>
+          </S.Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <S.BackToSignButton onPress={() => {}}>
+        <Icon name="arrow-left" size={20} color="#0070ba" />
+        <S.BackToSignText>Do you have an account?</S.BackToSignText>
+      </S.BackToSignButton>
+    </>
+  );
 };
 
 export default SignUp;
--- app/src/pages/SignUp/styles.ts
+++ app/src/pages/SignUp/styles.ts
@@ -1,3 +1,42 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #0e0d11;
+  font-family: 'Roboto-Regular';
+  margin: 65px 0 25px;
+`;
+
+export const BackToSignButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  background: #0070ba;
+  color: #0e0d11;
+  background: #fff;
+  border-top-width: 1px;
+  border-color: #dfdfdf;
+  width: 100%;
+
+  /* Setting a dinamic space for iphones */
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const BackToSignText = styled.Text`
+  color: #0070ba;
+  font-size: 16px;
+  font-family: 'Roboto-Medium';
+  margin-left: 10px;
+`;
