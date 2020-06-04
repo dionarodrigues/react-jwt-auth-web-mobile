348eb8de3bf2fd68b6be3052b8f8f8cd93316da4
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-06-04 14:54:42 -0300

    Adding axios and finishing SignUp

--- app/src/pages/SignUp/index.tsx
+++ app/src/pages/SignUp/index.tsx
@@ -13,6 +13,7 @@ import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import api from '../../services/api';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -53,10 +54,15 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      console.log(data);
 
-      // Alert.alert('Successful registration!', 'You can now login. 😍');
-      // history.push('/');
+      await api.post('/users', data);
+
+      console.log('posted');
+
+      Alert.alert('Successful registration!', 'You can now login. 😍');
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValitationErrors(err);
